36c9ac1b917981d8d80f21906f17197bba3e1c88
Asad-Sa02 <asadsayyad208@gmail.com>
2025-09-08 11:59:15 +0530

    removed toast error and added redux for cart , button to add to cart

--- alfaengineeringworks/src/components/GetItems.jsx
+++ alfaengineeringworks/src/components/GetItems.jsx
@@ -1,10 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { getItems } from '../services/items'
 import { toast } from 'react-toastify'
+import { useDispatch } from 'react-redux'
+import { addItem } from '../features/cartSlice'
 
 export default function GetItems() {
   const [items, setItems] = useState([])
 
+  function ItemRow ({item}){
+  const dispatch = useDispatch();
+    const addItemtocart = () => {
+    console.log(item)
+    dispatch(addItem(item))
+  } 
+    return (
+   <tr>
+      <td>{item.ITEM_ID}</td>
+      <td>{item.ITEM}</td>
+      <td>₹{item.PRICE}</td>
+      <td>{item.size}</td>
+     
+      <td><button onClick={addItemtocart}>
+  add to cart
+</button></td>
+
+    </tr>
+  )}
+  
   useEffect(()=>{
     const  loadItems = async ()=>{
     const result = await getItems();
@@ -15,18 +37,18 @@ export default function GetItems() {
             setItems(result.data);
  
         }
-        else{
-            toast.error(result.error);
-        }
     }
     loadItems();
   },[])
-    return (
-    <div>
-      <p>item display</p>
+    return (<>
       {items.map((item)=>(
-        <p>{item.ITEM}</p>
+      <>
+        
+          <ItemRow item={item}
+          key={item.ITEM_ID}
+          />
+            
+        </>
       ))}
-    </div>
-  )
+  </>  )
 }
