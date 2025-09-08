423a59a6a981caf542b9336d6fe1231319bc102d
Asad-Sa02 <asadsayyad208@gmail.com>
2025-09-08 12:26:54 +0530

    cart component

--- alfaengineeringworks/src/pages/Cart.jsx
+++ alfaengineeringworks/src/pages/Cart.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
+import { useSelector } from 'react-redux'
 
 export default function Cart() {
+  const selector = useSelector(state => state.cart);
+  // console.log('cart obj',selector)
   return (
-    <div>
-      <li></li>
-    </div>
+    <>
+    <Navbar/>
+    </>
   )
 }
